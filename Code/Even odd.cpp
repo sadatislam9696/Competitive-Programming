f1a63fa030211e490c6f9e69fc0bc1f77f7ce641
sadatislam9696 <122301741+sadatislam9696@users.noreply.github.com>
2023-01-11 15:41:58 +0600

    Add files via upload

---
+++ Code/Even odd.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+    int num;
+    cout<<"Enter an integer : ";
+    cin>>num;
+
+    if (num%2==0)
+    {
+        cout<<"Even";
+    }
+    else
+    {
+        cout<<"Odd";
+    }
+
+
+}
