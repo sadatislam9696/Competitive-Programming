286bedc05abc58f8a962170083df3f6188719863
sadatislam9696 <122301741+sadatislam9696@users.noreply.github.com>
2023-01-11 19:56:35 +0600

    Add files via upload

---
+++ Code/Multiplication Table .cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using  namespace std;
+int main()
+{
+    int num,i ;
+    cout<<"Enter any number : ";
+    cin>>num;
+    for(i=1; i<=10; i++)
+    {
+        cout<<num<<"x"<<i<<" = "<<(num*i)<<endl;
+    }
+
+}
