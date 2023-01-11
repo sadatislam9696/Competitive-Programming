423fcc7f51c69dfd55762e97fcb2bef6e4a1880e
sadatislam9696 <122301741+sadatislam9696@users.noreply.github.com>
2023-01-11 19:23:36 +0600

    Add files via upload

---
+++ Code/Break statement.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for (int i = 1; i<=100; i++)
+    {
+        if (i==10)
+        {
+            break;/////////use of break {switch & loop}
+        }
+
+        cout<<i<<endl;
+    }
+
+    for(int i = 1; i<=100; i++)
+    {
+
+        if(i==10,i==19)
+        {
+            continue;//////////use of continue{just loop}
+        }
+        cout<<i<<endl;
+    }
+
+    for (int i=1;i<=100;i = i+3)
+    {
+        if (i==10)
+        {
+            continue;
+        }
+        if (i==13)
+        {
+            break;
+
+        }
+        cout<<i<<endl;
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
