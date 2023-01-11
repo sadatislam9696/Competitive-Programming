2d3e14ca651b52a4e2a7dcd6b70e0e1d8cfb2dbc
sadatislam9696 <122301741+sadatislam9696@users.noreply.github.com>
2023-01-11 17:35:46 +0600

    Add files via upload

---
+++ Code/Difference of while loop and do while ioop.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ////while loop
+    int x = 10;
+    while (x<5)
+    {
+        cout<<"Hello ";
+        x++;
+    }
+    //do while loop
+   {
+    int x  = 10;
+    do
+    {
+        cout<<"Hello";
+        x++;
+
+    }while(x<5);
+   }
+}
