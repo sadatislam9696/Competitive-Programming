dd264e5ca1567b017fc5710091677d535360452c
sadatislam9696 <122301741+sadatislam9696@users.noreply.github.com>
2023-01-12 17:24:11 +0600

    Add files via upload

---
+++ Code/Prime number .c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    while(1)
+    {
+        int num,i,count =0;
+        printf("Enter any positive number : ");
+        scanf("%d",&num);
+
+        for(i=2; i<num; i++)
+        {
+            if (num%i==0)
+            {
+                count++;
+                break;
+            }
+        }
+        if(count==0)
+            printf("It is prime number \n");
+        else
+            printf("Not prime number \n");
+    }
+
+
+}
