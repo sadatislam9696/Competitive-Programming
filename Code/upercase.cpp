4a13edb44ee4ef54190d711491cc243af8d36535
sadatislam9696 <122301741+sadatislam9696@users.noreply.github.com>
2023-02-22 01:08:46 +0600

    Add files via upload

---
+++ Code/upercase.cpp
@@ -0,0 +1,18 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{  /*input : aPPle
+    output: APPle
+    mane prothom digit
+    ta capital hoiya jabe
+    towupper case babohar
+    korar karone*/
+
+    string s;
+    cin>>s;
+
+    s[0]=towupper(s[0]);
+    cout<<s;
+    return 0;
+}
