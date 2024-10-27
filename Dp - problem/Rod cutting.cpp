db4c164a7a32b7d6ded637a089c254f833f8aace
sadat islam abdullah <122301741+sadatislam9696@users.noreply.github.com>
2024-10-28 00:41:55 +0600

    Create Rod cutting.cpp

---
+++ Dp - problem/Rod cutting.cpp
@@ -0,0 +1,130 @@
+/***   Bismillahir Rahmanir Rahim   ***/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define     nn          '\n'
+#define     ss          second
+#define     ff          first
+#define     sp(d, n)    fixed << setprecision(d) << n
+#define     srt(v)      sort(v.begin(),v.end());
+#define     rsrt(v)     sort(v.rbegin(),v.rend());
+#define     rev(v)      reverse(v.begin(),v.end());
+#define     mem(a, x)   memset(a, x, sizeof a)
+
+using ll =  long long;
+using ld =  long double;
+using ull = unsigned long long;
+const ld pi = acos(-1.0);
+template <typename T>
+using minHeap = priority_queue<T, vector<T>, greater<T>>;
+
+int dx[6] = {0, 1, -1, 0, 0};
+int dy[6] = {0, 0, 0, 1, -1};
+
+const int M = 1e9 + 7;
+const int N = 1e6 + 9;
+
+int ways[N];
+int find_ans(int n){
+   if(n == 0)return 1;
+   if(ways[n] != -1){
+    return ways[n];
+   }
+   int ans = 0;
+   for(int i = 1; i <= n; i++){
+      ans += find_ans(n - i);
+   }
+   ways[n] = ans;
+
+   return ans;
+}
+void solution(){
+    int n; 
+    cin >> n;
+    memset(ways, -1, sizeof(ways));
+    cout << find_ans(n) << nn;
+}
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);cout.tie(nullptr);
+
+    int t = 1, c = 1;
+    cin >> t;
+    //cin.ignore();
+    while(t--){
+        //cout << "Case " << c++ << ": ";
+        solution();
+    }
+    return 0;
+}
+
+
+
+ 
+/***   Bismillahir Rahmanir Rahim   ***/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define     nn          '\n'
+#define     ss          second
+#define     ff          first
+#define     sp(d, n)    fixed << setprecision(d) << n
+#define     srt(v)      sort(v.begin(),v.end());
+#define     rsrt(v)     sort(v.rbegin(),v.rend());
+#define     rev(v)      reverse(v.begin(),v.end());
+#define     mem(a, x)   memset(a, x, sizeof a)
+
+using ll =  long long;
+using ld =  long double;
+using ull = unsigned long long;
+const ld pi = acos(-1.0);
+template <typename T>
+using minHeap = priority_queue<T, vector<T>, greater<T>>;
+
+int dx[6] = {0, 1, -1, 0, 0};
+int dy[6] = {0, 0, 0, 1, -1};
+
+const int M = 1e9 + 7;
+const int N = 1e6 + 9;
+
+int ways[N];
+int find_ans(int n){
+   if(n == 0)return 1;
+   if(ways[n] != -1){
+    return ways[n];
+   }
+   int ans = 0;
+   for(int i = 1; i <= n; i++){
+      ans += find_ans(n - i);
+   }
+   ways[n] = ans;
+
+   return ans;
+}
+void solution(){
+    int n; 
+    cin >> n;
+    memset(ways, -1, sizeof(ways));
+    cout << find_ans(n) << nn;
+}
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);cout.tie(nullptr);
+
+    int t = 1, c = 1;
+    cin >> t;
+    //cin.ignore();
+    while(t--){
+        //cout << "Case " << c++ << ": ";
+        solution();
+    }
+    return 0;
+}
+
+
+
+ 
